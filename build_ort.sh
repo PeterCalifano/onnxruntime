71b33378dab107c8327d47ee6764e409fbab4cb5
PeterC <petercalifano.gs@gmail.com>
2025-10-25 18:36:43 +0200

    Add new input args to build with new EPs

--- build_ort.sh
+++ build_ort.sh
@@ -1,5 +1,11 @@
 # Ensure dependencies are installed
 sudo apt install libgtest-dev libgmock-dev cmake
 
-# Call build script
-./build.sh --config RelWithDebInfo --build_shared_lib --parallel --compile_no_warning_as_error #--skip_submodule_sync
+# Call build script passing default args
+
+./build.sh --config RelWithDebInfo --build_shared_lib --parallel --compile_no_warning_as_error --use_cuda #--use_dnnl --use_openvino --use_tensorrt --use_tensorrt_builtin_parser
+
+
+
+# Other options that could be useful
+#--skip_submodule_sync --enable_nvtx_profile --enable_cuda_profiling
