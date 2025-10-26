f3de64d2fce5cec602831f6a540a7ffd4db426cd
PeterC <petercalifano.gs@gmail.com>
2025-10-26 21:47:03 +0100

    Update build script

--- build_ort.sh
+++ build_ort.sh
@@ -3,7 +3,7 @@ sudo apt install libgtest-dev libgmock-dev cmake
 
 # Call build script passing default args
 
-./build.sh --config RelWithDebInfo --build_shared_lib --parallel --compile_no_warning_as_error --use_cuda #--use_dnnl --use_openvino --use_tensorrt --use_tensorrt_builtin_parser
+./build.sh --config RelWithDebInfo --build_shared_lib --parallel 2 --compile_no_warning_as_error --use_cuda #--use_dnnl --use_openvino --use_tensorrt --use_tensorrt_builtin_parser
 
 
 
